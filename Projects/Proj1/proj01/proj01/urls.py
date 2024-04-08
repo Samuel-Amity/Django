9da5b00943cd10e061e189285f38b670222de139
Samuel Nathan <144701270+Samuel-Amity@users.noreply.github.com>
2024-04-08 23:40:22 +0530

    proj hw

--- Projects/Proj1/proj01/proj01/urls.py
+++ Projects/Proj1/proj01/proj01/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app01.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home,)
 ]
