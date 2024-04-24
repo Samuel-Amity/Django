22ee54d018ae8c282f22bf4d4c1a18c324408c27
Samuel Nathan <144701270+Samuel-Amity@users.noreply.github.com>
2024-04-24 14:47:50 +0530

    innitial commit

---
+++ Projects/par/par/kar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kar'
---
+++ Projects/par/par/kar/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('',views.home),
+    path('about/',views.about,name="about"),
+    path('category/',views.category,name="category"),
+    path("category/<slug:val>",views.CategoryView.as_view(),name="category"),
+    path("category-title/<val>",views.CategoryTitle.as_view(),name="category-title"),
+    path("product-detail/<int:pk>",views.ProductDetail.as_view(),name="product"),
+    path("registration/<slug:val>",views.RegistrationView.as_view(),name="registration"),
+    
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
